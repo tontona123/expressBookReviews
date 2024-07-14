17f5c05e35527052086badd04d0c220cfd36435f
AFRA APACHE <68964470+siamwebsite@users.noreply.github.com>
2024-07-15 04:09:07 +0700

    Update code to the latest version

--- final_project/index.js
+++ final_project/index.js
@@ -1,22 +1,34 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
-const session = require('express-session')
+const session = require('express-session');
 const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
 
 const app = express();
 
 app.use(express.json());
+app.use(session({ secret: "fingerprint_customer", resave: true, saveUninitialized: true }));
 
-app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
-
-app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+// Middleware for authentication
+app.use("/auth/*", function auth(req, res, next) {
+    if (req.session.authorization) {
+        token = req.session.authorization['accessToken'];
+        jwt.verify(token, "access", (err, user) => {
+            if (!err) {
+                req.user = user;
+                next();
+            } else {
+                return res.status(403).json({ message: "User not authenticated" });
+            }
+        });
+    } else {
+        return res.status(403).json({ message: "User not logged in" });
+    }
 });
- 
-const PORT =5000;
 
-app.use("/customer", customer_routes);
+const PORT = 5000;
+
 app.use("/", genl_routes);
+app.use("/", customer_routes);
 
-app.listen(PORT,()=>console.log("Server is running"));
+app.listen(PORT, () => console.log("Server is running"));
